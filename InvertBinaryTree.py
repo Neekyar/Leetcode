ee1339ad49adf1c8267b3b2c6a75b667d2ddcf9e
Sun Jacques <112730692+SunJacques@users.noreply.github.com>
2022-10-17 19:56:29 +0200

    Update InvertBinaryTree.py

--- InvertBinaryTree.py
+++ InvertBinaryTree.py
@@ -1,10 +1,7 @@
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        i = 1
-        while 2**i < len(root):
-            for j in range(2**(i-1)):
-                root[2**i - 1 + j],root[2**i - 1 + 2**(i-1) + j] = root[2**i - 1 + 2**(i-1)+j], root[2**i - 1 + j]
-
+        if root:
+            root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
         return root
 
     
