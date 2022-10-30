10f694db45c153bc5334077ece9ab5be6843210d
Sun Jacques <112730692+SunJacques@users.noreply.github.com>
2022-10-30 21:26:06 +0100

    Update Merge list.py

--- Merge list.py
+++ Merge list.py
@@ -4,11 +4,11 @@ class Solution:
         while list1 and list2:
             if list1.val() < list2.val():
                 cur.next = list1
-                list1, cur = list1.next, list1
+                list1 = list1.next
             else:
                 cur.next = list2
-                list2, cur = list2.next, list2
-
+                list2= list2.next
+            cur = cur.next
         cur.next = list1 or list2
         return dummy.next
             
